423c5664fb473000cf9e0010fd699349089c61e0
Nayara Zahn <42813430+nanyzahn@users.noreply.github.com>
2019-07-04 23:52:11 -0300

    Update contact_me.php

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -11,7 +11,7 @@ $phone = strip_tags(htmlspecialchars($_POST['phone']));
 $message = strip_tags(htmlspecialchars($_POST['message']));
 
 // Create the email and send the message
-$to = "nanyzahn@hotmail.com"; // Add your email address inbetween the "" replacing yourname@yourdomain.com - This is where the form will send a message to.
+$to = "nanyzahn@gmail.com"; // Add your email address inbetween the "" replacing yourname@yourdomain.com - This is where the form will send a message to.
 $subject = "Contatos Através do Site:  $name";
 $body = "Você recebeu um nova mensagem do seu site.\n\n"."Detalhes:\n\nName: $name\n\nEmail: $email\n\nPhone: $phone\n\nMessage:\n$message";
 $header = "From: noreply@yourdomain.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
